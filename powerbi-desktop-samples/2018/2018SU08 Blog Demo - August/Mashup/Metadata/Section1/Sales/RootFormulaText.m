4403335351f1abba485d6caa15f430bf0cee93ba
Mathias Thierbach <mt-dev@outlook.com>
2019-03-05 23:27:35 +0000

    powerbi-desktop-samples/2018/2018SU08

---
+++ powerbi-desktop-samples/2018/2018SU08 Blog Demo - August/Mashup/Metadata/Section1/Sales/RootFormulaText.m
@@ -0,0 +1,12 @@
+let
+    Source = Excel.Workbook(File.Contents("C:\Users\amac\Documents\2016 09 06 Contoso - Sales.xlsx"), null, true),
+    Sales_Table = Source{[Item="Sales",Kind="Table"]}[Data],
+    #"Changed Type" = Table.TransformColumnTypes(Sales_Table,{{"SalesOrderID", type text}, {"Country", type text}, {"OrderDate", type date}, {"SalesChannelCode", type text}, {"ProdID", Int64.Type}, {"StyleName", type text}, {"CustomerAccountNumber", type text}, {"StoreKey", Int64.Type}, {"ProductLabel", Int64.Type}, {"ProductName", type text}, {"ProductDescription", type text}, {"Manufacturer", type text}, {"BrandName", type text}, {"Class", type text}, {"Color", type text}, {"StockType", type text}, {"Units", Int64.Type}, {"SalesAmount", type number}, {"PurchAgain", Int64.Type}, {"NSAT", Int64.Type}, {"SubCategory", type text}, {"Category", type text}}),
+    #"Added Conditional Column" = Table.AddColumn(#"Changed Type", "Custom", each if [SalesAmount] >= 5000 then "Large" else if [SalesAmount] >= 1000 then "Medium" else "Small"),
+    #"Renamed Columns" = Table.RenameColumns(#"Added Conditional Column",{{"Custom", "Sale Size"}, {"PurchAgain", "RePurch"}}),
+    #"Split Column by Delimiter" = Table.SplitColumn(#"Renamed Columns", "ProductName", Splitter.SplitTextByEachDelimiter({" "}, QuoteStyle.Csv, true), {"ProductName.1", "ProductName.2"}),
+    #"Changed Type1" = Table.TransformColumnTypes(#"Split Column by Delimiter",{{"ProductName.1", type text}, {"ProductName.2", type text}}),
+    #"Removed Columns" = Table.RemoveColumns(#"Changed Type1",{"ProductName.2"}),
+    #"Renamed Columns1" = Table.RenameColumns(#"Removed Columns",{{"ProductName.1", "ProductName"}})
+in
+    #"Renamed Columns1"
